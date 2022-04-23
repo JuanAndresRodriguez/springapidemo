30701d6299225240b99167f0106a457ba4e0f225
JuanAndresRodriguez <juan.rodriguezpedreira@gmail.com>
2022-04-22 23:27:50 -0300

    Upload to Master branch

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertDoesNotThrow(this::doNotThrowException);
+	}
+
+	private void doNotThrowException(){
+		//This method will never throw exception
+	}
+}
